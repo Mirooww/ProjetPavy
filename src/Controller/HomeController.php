7e9a4efabaaf133d878a6e30713c99bdc4fef9a9
[Mirooww] <[jordanlansoypro@gmail.com]>
2023-12-22 17:18:54 +0100

    Mise à jour entity

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
